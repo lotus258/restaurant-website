2089a545927d04635572a668656ccbc3976303f1
lotus258 <liyan0626@gmail.com>
2025-02-06 23:10:19 -0700

    Added lemon symbol to footer.

--- frontend/my-app/src/Components/Footer.js
+++ frontend/my-app/src/Components/Footer.js
@@ -12,10 +12,14 @@ function Footer() {
                 <Nav />
             </div> */}
             <div className="contact">
+                <ul><br></br></ul>
+                <ul><br></br></ul>
+                <ul>🍋🍋🍋</ul>
                 <ul>Contact:<br></br> 888-888-8888</ul>
                 <ul>Email: <br></br>littlelemon@gmail.com</ul>
                 <ul>Address: <br></br>12500 S 1300 E, Draper, UT 84020</ul>
                 <ul>Follow us on: <br></br>Facebook</ul>
+                <ul><br></br></ul>
             </div>
         </div>
     )
