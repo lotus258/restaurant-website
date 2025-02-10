d5d60fdcf92092e8571f8d0118a4634228f7e58d
lotus258 <liyan0626@gmail.com>
2025-02-10 11:27:20 -0700

    Updated backend tests due to updated model.

--- backend/restaurant/tests/test_models.py
+++ backend/restaurant/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-from restaurant.models import Menu
+from restaurant.models import Menu, Category
 
 class TestMenu(TestCase):
     def test_get_item(self):
-        item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(str(item), "IceCream : 80")
+        category = Category.objects.create(title="dessert", slug="dessert")
+        item = Menu.objects.create(title="IceCream", price=4.5, inventory=100, category=category)
+        self.assertEqual(str(item), "IceCream : 4.5")
--- backend/restaurant/tests/test_views.py
+++ backend/restaurant/tests/test_views.py
@@ -1,19 +1,21 @@
 from django.test import TestCase
-from restaurant.models import Menu
+from restaurant.models import Menu, Category
 from restaurant.serializers import MenuItemSerializer
 
 class MenuViewTest(TestCase):
     def setUp(self):
-        self.menu_item1 = Menu.objects.create(title="Pizza", price=10.99, inventory=50)
-        self.menu_item2 = Menu.objects.create(title="Noodle", price=3.45, inventory=30)
-        self.menu_item3 = Menu.objects.create(title="Pasta", price=12.99, inventory=20)
+        self.category_1 = Category.objects.create(slug="noodle", title="noodle")
+        self.category_2 = Category.objects.create(slug="drink", title="drink")
+        self.menu_item1 = Menu.objects.create(title="Shio Ramen", price=10.99, inventory=50, category=self.category_1)
+        self.menu_item2 = Menu.objects.create(title="Tantanmen Ramen", price=13.45, inventory=30, category=self.category_1)
+        self.menu_item3 = Menu.objects.create(title="Sprite", price=2.99, inventory=20, category=self.category_2)
 
     def test_getall(self):
         menu_items = Menu.objects.all()
         serializer = MenuItemSerializer(menu_items, many=True)
         expected_data = [
-            {"id": self.menu_item1.id, "title": "Pizza", "price": "10.99", "inventory": 50},
-            {"id": self.menu_item2.id, "title": "Noodle", "price": "3.45", "inventory": 30},
-            {"id": self.menu_item3.id, "title": "Pasta", "price": "12.99", "inventory": 20},
+            {"id": self.menu_item1.id, "title": "Shio Ramen", "price": "10.99", "inventory": 50, "category":2},
+            {"id": self.menu_item2.id, "title": "Tantanmen Ramen", "price": "13.45", "inventory": 30, "category":2},
+            {"id": self.menu_item3.id, "title": "Sprite", "price": "2.99", "inventory": 20, "category":3},
         ]
         self.assertEqual(serializer.data, expected_data)
